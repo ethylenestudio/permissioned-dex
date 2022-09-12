99efc93742d2ce34349e24caf3b470aca46e1f8d
Ulaş Erdoğan <uerdogan2001@hotmail.com>
2022-09-12 21:55:30 +0300

    batches

--- blockchain/contracts/CrocPermitOracle.sol
+++ blockchain/contracts/CrocPermitOracle.sol
@@ -1,63 +1,101 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.9;
 
+import "./ICrocPermitOracle.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CrocPermitOracle is Ownable {
-    // swap - mint - burn - init
-    mapping(address => bool[4]) public auths;
+struct Auths {
+    bool s;
+    bool m;
+    bool b;
+    bool i;
+}
 
-    function setAuth(address user, bool[4] memory userAuths)
+contract CrocPermitOracle is ICrocPermitOracle, Ownable {
+    mapping(address => Auths) public auths;
+
+    function batchAuth(address[] memory user, Auths[] memory authList)
         external
         onlyOwner
     {
-        auths[user] = userAuths;
+        for (uint256 i; i < user.length; i++) {
+            auths[user[i]] = authList[i];
+        }
+    }
+
+    function setAuth(address user, Auths memory auth) external onlyOwner {
+        auths[user] = auth;
     }
 
     function checkApprovedForCrocSwap(
         address user,
-        address,
-        address,
-        address,
-        bool,
-        bool,
-        uint128,
-        uint16
-    ) external view returns (uint16) {
-        return auths[user][0] ? uint16(1) : uint16(0);
+        address sender,
+        address base,
+        address quote,
+        bool isBuy,
+        bool inBaseQty,
+        uint128 qty,
+        uint16 poolFee
+    ) external view override returns (uint16) {
+        return auths[user].s ? uint16(1) : uint16(0);
     }
 
     function checkApprovedForCrocMint(
         address user,
-        address,
-        address,
-        address,
-        int256,
-        int256,
-        uint16
-    ) external view returns (bool) {
-        return auths[user][1];
+        address sender,
+        address base,
+        address quote,
+        int24 bidTick,
+        int24 askTick,
+        uint128 liq
+    ) external view override returns (bool) {
+        return auths[user].m;
     }
 
     function checkApprovedForCrocBurn(
         address user,
-        address,
-        address,
-        address,
-        int256,
-        int256,
-        uint16
-    ) external view returns (bool) {
-        return auths[user][2];
+        address sender,
+        address base,
+        address quote,
+        int24 bidTick,
+        int24 askTick,
+        uint128 liq
+    ) external view override returns (bool) {
+        return auths[user].b;
     }
 
     function checkApprovedForCrocInit(
         address user,
-        address,
-        address,
-        address,
-        uint256
-    ) external view returns (bool) {
-        return auths[user][3];
+        address sender,
+        address base,
+        address quote,
+        uint256 poolIdx
+    ) external view override returns (bool) {
+        return auths[user].i;
     }
 }
+
+/** can be used for bool storing
+
+    function getBoolean(uint256 _packedBools, uint256 _boolNumber)
+        public view returns(bool)
+    {
+        uint256 flag = (_packedBools >> _boolNumber) & uint256(1);
+        return (flag == 1 ? true : false);
+    }
+
+
+    function setBoolean(
+        uint256 _packedBools,
+        uint256 _boolNumber,
+        bool _value
+    ) public returns(uint256) {
+        if (_value)
+            _packedBools = _packedBools | uint256(1) << _boolNumber;
+            return _packedBools;
+        else
+            _packedBools = _packedBools & ~(uint256(1) << _boolNumber);
+            return _packedBools;
+    }
+
+ */
